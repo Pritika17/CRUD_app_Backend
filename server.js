c2db45a3e75e0351520c9d2cf2838c3ac267c1b4
pritika mishra <pritikamishra2017@gmail.com>
2023-01-09 23:44:16 +0530

    fixed a bug

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./app")
 const PORT = process.env.PORT || 4000
 
 app.listen(PORT, () => {
-    console.log(`App is running at http://localhost:${PORT}`)
+    console.log(`App is running at https://localhost:${PORT}`)
 })
