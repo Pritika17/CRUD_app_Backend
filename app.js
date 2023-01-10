5bfca33e31c7fc30d637fc2cdd741ff5d28db5de
pritika mishra <pritikamishra2017@gmail.com>
2023-01-10 12:25:13 +0530

    some changes

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const userRoutes = require("./routes/userRoutes")
 //Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 connectToDB()
 app.use("/", userRoutes)
